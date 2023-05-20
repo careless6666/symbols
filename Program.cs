5cfb45471f6376755d42a010cdb2f96ad688f99c
alexander.usov <alexander.usov@aliexpress.ru>
2023-05-20 13:09:31 +0300

    init

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+
+var app = builder.Build();
+
+app.MapControllers();
+app.MapGet("test_slowpoke", () =>
+{
+    long counter = 0;
+    for (var i = 0; i < 1_000_000_000; i++)
+    {
+        counter++;
+    }
+	
+    return "Hello world";
+});
+
+app.Run();
